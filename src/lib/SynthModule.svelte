04a04c4a159bc831e9458222120afeb64193f0e7
Nick Roberts <nick@labelbox.com>
2021-12-07 11:51:30 -0800

    began synth work

--- src/lib/SynthModule.svelte
+++ src/lib/SynthModule.svelte
@@ -1,3 +1,10 @@
 <script lang="ts">
     import {chords} from './store';
+    import Tone from 'tone'
+    const synth = new Tone.PolySynth(Tone.Synth).toDestination();
+    chords.subscribe((chords) => {
+        const currentChord = chords[chords.length - 1];
+        const chordNotes = ['A4'];
+        synth.triggerAttack(chordNotes);
+    })
 </script>
