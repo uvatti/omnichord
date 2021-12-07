da72c0e12c23c9c16f758b030a1a02882f1d0816
Nick Roberts <nick@labelbox.com>
2021-12-07 13:20:52 -0800

    fixed store bug

--- src/lib/Board.svelte
+++ src/lib/Board.svelte
@@ -1,103 +1,114 @@
 <script lang="ts">
-import { subscribe } from 'svelte/internal';
+  import { subscribe } from "svelte/internal";
 
-    import {chords} from './store';
-    
-    let subs;
-    const keyIn= (note:string, type:string)=>{
-       if(checkIfPressed(note,type)){
-        chords.chordReleased({note,type});
-       }else{
-        chords.chordPressed({note,type});
-       }
+  import { Chord, chords } from "./store";
+
+  let subs;
+  const keyIn = (chord: Chord) => {
+    if (checkIfPressed(chord)) {
+      console.log("released");
+      chords.chordReleased(chord);
+    } else {
+      console.log("pressed");
+      chords.chordPressed(chord);
     }
-    const checkIfPressed = (note:string, type:string)=>{
-        return subs.filter((keys)=>{return keys.note === note && keys.type === type}).length > 0;
-    }
+  };
+  const checkIfPressed = ({ note, type }: Chord) => {
+    return (
+      subs.filter((keys) => {
+        return keys.note === note && keys.type === type;
+      }).length > 0
+    );
+  };
 
-    chords.subscribe(value => {
-		subs = value;
-        console.log(value);
-	});
+  chords.subscribe((value) => {
+    subs = value;
+    console.log(value);
+  });
 
-    const keys= ["Eb","Bb","F","C","G","D","A","E","B"];
-  </script>
+  const keys = ["Eb", "Bb", "F", "C", "G", "D", "A", "E", "B"];
+</script>
 
-
-  <div class="board-wrapper">
-     <div class="major note">
-        {#each keys as k }
-        <div class="board-button" on:click={()=>keyIn("major",k)}>
-            {k}</div>
-        {/each}
-       
-     </div>
-     <div class="minor note">
-        {#each keys as k }
-        <div class="board-button" on:click={()=>keyIn("minor",k)}>
-            {k}</div>
-        {/each}
-    </div>
-    <div class="th note">
-        {#each keys as k }
-        <div class="board-button" on:click={()=>keyIn("7th",k)}>
-            {k}</div>
-        {/each}
-    </div>
-    <button on:click={()=>console.log(chords.subscribe)}>
-    SUBS</button>
-
+<div class="board-wrapper">
+  <div class="major note">
+    {#each keys as k}
+      <div
+        class="board-button"
+        on:click={() => keyIn({ note: k, type: "major" })}
+      >
+        {k}
+      </div>
+    {/each}
   </div>
-  
-  <style>
-      .board-wrapper{
-        display: flex;
+  <div class="minor note">
+    {#each keys as k}
+      <div
+        class="board-button"
+        on:click={() => keyIn({ note: k, type: "minor" })}
+      >
+        {k}
+      </div>
+    {/each}
+  </div>
+  <div class="th note">
+    {#each keys as k}
+      <div
+        class="board-button"
+        on:click={() => keyIn({ note: k, type: "min7" })}
+      >
+        {k}
+      </div>
+    {/each}
+  </div>
+  <button on:click={() => console.log(chords.subscribe)}> SUBS</button>
+</div>
+
+<style>
+  .board-wrapper {
+    display: flex;
     flex-direction: column;
     align-items: center;
-      }
-    .note{
-        display: flex;
-        flex-direction: row;
-        
-    }
-    .note.major{
+  }
+  .note {
+    display: flex;
+    flex-direction: row;
+  }
+  .note.major {
+  }
+  .note.minor {
+    padding-left: 60px;
+  }
+  .note.th {
+    padding-left: 120px;
+  }
+  .board-button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
+    width: 50px;
+    height: 50px;
+    background: gray;
+  }
+  button {
+    font-family: inherit;
+    font-size: inherit;
+    padding: 1em 2em;
+    color: #ff3e00;
+    background-color: rgba(255, 62, 0, 0.1);
+    border-radius: 2em;
+    border: 2px solid rgba(255, 62, 0, 0);
+    outline: none;
+    width: 200px;
+    font-variant-numeric: tabular-nums;
+    cursor: pointer;
+  }
 
-    }
-    .note.minor{
-        padding-left: 60px;
-    }
-    .note.th{
-        padding-left: 120px;
-    }
-    .board-button{
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        margin: 10px;
-        width:50px;
-        height: 50px;
-        background:gray;
-    }
-    button {
-      font-family: inherit;
-      font-size: inherit;
-      padding: 1em 2em;
-      color: #ff3e00;
-      background-color: rgba(255, 62, 0, 0.1);
-      border-radius: 2em;
-      border: 2px solid rgba(255, 62, 0, 0);
-      outline: none;
-      width: 200px;
-      font-variant-numeric: tabular-nums;
-      cursor: pointer;
-    }
-  
-    button:focus {
-      border: 2px solid #ff3e00;
-    }
-  
-    button:active {
-      background-color: rgba(255, 62, 0, 0.2);
-    }
-  </style>
-  
+  button:focus {
+    border: 2px solid #ff3e00;
+  }
+
+  button:active {
+    background-color: rgba(255, 62, 0, 0.2);
+  }
+</style>
