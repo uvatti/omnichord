69222eb92b804a227cc0636d005df8106a06c587
Nick Roberts <nick@labelbox.com>
2021-12-07 11:37:34 -0800

    began work on chord store

---
+++ src/lib/store.ts
@@ -0,0 +1,17 @@
+import { writable } from "svelte/store";
+
+export type Chord = { note: string; type: string };
+export const chordStore = () => {
+  const { subscribe, update } = writable([]);
+
+  return {
+    subscribe,
+    chordPressed: (chord: Chord) => update((chords) => [...chords, chord]),
+    chordReleased: ({ note, type }: Chord) =>
+      update((chords) =>
+        chords.filter((c) => c.note === note && c.type === type)
+      ),
+  };
+};
+
+export const chords = chordStore();
